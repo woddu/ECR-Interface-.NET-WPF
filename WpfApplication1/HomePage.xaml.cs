e37aa27e2202ca8991b0044805d45e39975df04c
woddu <fwoddu@yahoo.com>
2025-09-01 00:30:30 +0800

    added filtering for scores textboxes, change in textboxes validation

--- WpfApplication1/HomePage.xaml.cs
+++ WpfApplication1/HomePage.xaml.cs
@@ -5,59 +5,52 @@ using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 
-namespace WpfApplication1
-{
-    /// <summary>
-    /// Interaction logic for HomePage.xaml
-    /// </summary>
-    public partial class HomePage : UserControl
-    {
+namespace WpfApplication1 {
+  /// <summary>
+  /// Interaction logic for HomePage.xaml
+  /// </summary>
+  public partial class HomePage : UserControl {
 
-        public event EventHandler<string[]> FileChosen;
+    public event EventHandler<string[]> FileChosen;
 
-        public HomePage()
-        {
-          InitializeComponent();
+    public HomePage() {
+      InitializeComponent();
     }
 
-        private void ChooseFile_Click(object sender, RoutedEventArgs e)
+    private void ChooseFile_Click(object sender, RoutedEventArgs e) {
+      var dialog = new OpenFileDialog {
+        Title = "Select an Excel file",
+        Filter = "Excel Files (*.xlsx;*.xls)|*.xlsx;*.xls"
+      };
+
+      if (dialog.ShowDialog() == true) // true means user clicked OK
+      {
+
+        FileChosen?.Invoke(this, new string[]
         {
-            var dialog = new OpenFileDialog
-            {
-                Title = "Select an Excel file",
-                Filter = "Excel Files (*.xlsx;*.xls)|*.xlsx;*.xls"
-            };
-
-            if (dialog.ShowDialog() == true) // true means user clicked OK
-            {
-
-                FileChosen?.Invoke(this, new string[]
-                {
                     dialog.FileName,
                     Path.GetFileNameWithoutExtension(dialog.FileName)
-                });
-            }
-        }
+        });
+      }
+    }
 
-        public void ShowError(string title, string error)
-        {
-            MessageBox.Show(
-                "Error: " + error,          // Title bar text
-                title,    // Message text
-                MessageBoxButton.OK,        // Buttons to display
-                MessageBoxImage.Error       // Icon type
-            );
-        }
+    public void ShowError(string title, string error) {
+      MessageBox.Show(
+          "Error: " + error,          // Title bar text
+          title,    // Message text
+          MessageBoxButton.OK,        // Buttons to display
+          MessageBoxImage.Error       // Icon type
+      );
+    }
 
-        public void SetFileName(string fileName)
-        {
-            FileName.Content = fileName;
-            btnChooseFile.Content = "Choose a Different File";
-        }
+    public void SetFileName(string fileName) {
+      FileName.Text = fileName;
+      btnChooseFile.Content = "Choose a Different File";
+    }
 
     public void SetLoading(bool isLoading) {
       ButtonProgressAssist.SetIsIndeterminate(btnChooseFile, isLoading);
     }
-    
+
   }
 }
--- WpfApplication1/IndexToQuizLabelConverter.cs
+++ WpfApplication1/IndexToQuizLabelConverter.cs
@@ -6,7 +6,7 @@ namespace WpfApplication1 {
   class IndexToQuizLabelConverter : IValueConverter {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
       if (value is int index) {
-        return $"Quiz #{index + 1}";
+        return $"#{index + 1}";
       }
       return "";
     }
