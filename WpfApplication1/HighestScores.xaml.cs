fa823a28a49457b540bf51418a83502c7081503b
woddu <fwoddu@yahoo.com>
2025-09-04 23:24:46 +0800

    added save functions for exam, writtenworks, performance tasks on validation wether the value(s) are different from the excel

--- WpfApplication1/HighestScores.xaml.cs
+++ WpfApplication1/HighestScores.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace WpfApplication1 {
   /// <summary>
@@ -10,24 +12,77 @@ namespace WpfApplication1 {
   /// </summary>
   public partial class HighestScores : UserControl {
 
+
+
     public ObservableCollection<string> WrittenWorks { get; set; } = new ObservableCollection<string>();
 
     public ObservableCollection<string> PerformanceTasks { get; set; } = new ObservableCollection<string>();
 
-    private string Exam { get; set; }
+    private string _exam;
+
+    public string Exam {
+      get { return _exam; }
+      set {
+        txtExam.Text = _exam = value;
+      }
+    }
+
+    public EventHandler SaveExamClicked;
+    public EventHandler SaveWrittenWorksClicked;
+    public EventHandler SavePerformanceTasksClicked;
 
     public HighestScores() {
       InitializeComponent();
       DataContext = this;
     }
 
-    public void SetWrittenWorkSaveEnabled(bool isEnabled) {
-      btnSaveWrittenWorks.IsEnabled = isEnabled;
-    }
-    public void SetPerformanceTaskSaveEnabled(bool isEnabled) {
-      btnSavePerformanceTasks.IsEnabled = isEnabled;
+    private void SaveExam_Click(object sender, System.Windows.RoutedEventArgs e) =>
+      SaveExamClicked?.Invoke(this, EventArgs.Empty);
+    
+
+    private void SaveWrittenWorks_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+      for (int i = 0; i < WrittenWorks.Count; i++) {
+        // Get the container for this item
+        var container = (ContentPresenter)itemsControlWrittenWorks
+            .ItemContainerGenerator.ContainerFromIndex(i);
+
+        if (container != null) {
+          // Find the TextBox inside the DataTemplate
+          var tb = FindVisualChild<TextBox>(container);
+          if (tb != null) {
+            WrittenWorks[i] = tb.Text; // overwrite the value in the collection
+          }
+        }
+      }
+
+      Debug.WriteLine("WrittenWorks saved: " + string.Join(", ", WrittenWorks));
+
+      SaveWrittenWorksClicked?.Invoke(this, EventArgs.Empty);
+      btnSaveWrittenWorks.IsEnabled = false;
     }
 
+    private void SavePerformanceTasks_Click(object sender, System.Windows.RoutedEventArgs e) {
+
+      for (int i = 0; i < WrittenWorks.Count; i++) {
+        // Get the container for this item
+        var container = (ContentPresenter)itemsControlPerformanceTasks
+            .ItemContainerGenerator.ContainerFromIndex(i);
+
+        if (container != null) {
+          // Find the TextBox inside the DataTemplate
+          var tb = FindVisualChild<TextBox>(container);
+          if (tb != null) {
+            PerformanceTasks[i] = tb.Text; // overwrite the value in the collection
+          }
+        }
+      }
+
+      Debug.WriteLine("PerformanceTasks saved: " + string.Join(", ", PerformanceTasks));
+
+      SavePerformanceTasksClicked?.Invoke(this, EventArgs.Empty);
+      btnSavePerformanceTasks.IsEnabled = false;
+    }
     private void WrittenScoresTextChanged(object sender, TextChangedEventArgs e) {
       var tb = (TextBox)sender;
       int index = (int)tb.Tag; // index in the collection
@@ -49,16 +104,30 @@ namespace WpfApplication1 {
     }
 
     private void ExamTextChanged(object sender, TextChangedEventArgs e) {
-      btnSaveExam.IsEnabled = !(txtExam.Text != Exam);
+      btnSaveExam.IsEnabled = txtExam.Text != Exam;
     }
 
     private void NumberOnlyTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e) {
       e.Handled = !int.TryParse(e.Text, out _);
     }
 
-    public void SetExam(string exam) {
-      Exam = exam;
-      txtExam.Text = exam;
+    public void SetExamBtnEnabled(bool enabled) => btnSaveExam.IsEnabled = enabled;
+
+    public void SetWrittenWorksBtnEnabled(bool enabled) => btnSaveWrittenWorks.IsEnabled = enabled;
+
+    public void SetPerformanceTasksBtnEnabled(bool enabled) => btnSavePerformanceTasks.IsEnabled = enabled;
+
+    private T FindVisualChild<T>(DependencyObject parent) where T : DependencyObject {
+      for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++) {
+        var child = VisualTreeHelper.GetChild(parent, i);
+        if (child is T tChild)
+          return tChild;
+
+        var result = FindVisualChild<T>(child);
+        if (result != null)
+          return result;
+      }
+      return null;
     }
   }
 }
