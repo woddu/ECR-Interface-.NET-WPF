17310e4cba49e153a57441a07e692b5b122f147f
woddu <fwoddu@yahoo.com>
2025-09-18 16:35:53 +0800

    added eventhandlers for comboBox and radioButtons, set their visbility as collapsed in initialization

--- WpfApplication1/HomePage.xaml.cs
+++ WpfApplication1/HomePage.xaml.cs
@@ -12,11 +12,38 @@ namespace WpfApplication1 {
   public partial class HomePage : UserControl {
 
     public event EventHandler<string[]> FileChosen;
+    public event EventHandler<bool> QuarterChosen;
+    public event EventHandler<int> TrackChosen;
 
     public HomePage() {
       InitializeComponent();
+      spQuarter.Visibility = Visibility.Collapsed;
+      cBoxTracks.Visibility = Visibility.Visible;
     }
 
+    private void Track_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+      if (cBoxTracks.SelectedItem is ComboBoxItem selectedItem) {
+        int index = cBoxTracks.SelectedIndex;
+        TrackChosen?.Invoke(this, index);
+      }
+    }
+
+    public void ChangeQuarter(bool q) {
+      if (q) {
+        rbtn1st.IsChecked = true;
+        rbtn2nd.IsChecked = false;
+      } else {
+        rbtn2nd.IsChecked = true;
+        rbtn1st.IsChecked = false;
+      }
+    }
+
+    private void Change_Quarter(object sender, RoutedEventArgs e) {
+      if (sender is RadioButton rbutton) {
+        bool tagValue = bool.TryParse(rbutton.Tag?.ToString(), out var result) && result;
+        QuarterChosen?.Invoke(this, tagValue);
+      }
+    }
     private void ChooseFile_Click(object sender, RoutedEventArgs e) {
       var dialog = new OpenFileDialog {
         Title = "Select an Excel file",
@@ -46,6 +73,9 @@ namespace WpfApplication1 {
     public void SetFileName(string fileName) {
       FileName.Text = fileName;
       btnChooseFile.Content = "Choose a Different File";
+      btnChooseFile.IsEnabled = true;
+      spQuarter.Visibility = Visibility.Visible;
+      cBoxTracks.Visibility = Visibility.Visible;
     }
 
     public void SetLoading(bool isLoading) {
