6d8f360ad0f167148198a3f27587b1e7b87de63b
woddu <fwoddu@yahoo.com>
2025-10-14 23:50:50 +0800

    added converter for enabling buttons in higest scores based on if it has value

---
+++ WpfApplication1/ScoreToEnabledConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfApplication1 {
+  public class ScoreToEnabledConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+      // value is the TextBox.Text (string)
+      if (string.IsNullOrWhiteSpace(value as string))
+        return false;
+
+      if (int.TryParse(value.ToString(), out int score))
+        return score != 0;
+
+      return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+  }
+}
