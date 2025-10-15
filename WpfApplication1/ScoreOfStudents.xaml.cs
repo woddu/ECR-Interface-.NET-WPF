bda4f3b9817f6b1014d03aa3f8e93a4a9861ce95
woddu <fwoddu@yahoo.com>
2025-10-15 15:41:37 +0800

    added properties: inital values of the scores, type, higestscore and column index and method for NumberOnlyTextBox_PreviewTextInput

--- WpfApplication1/ScoreOfStudents.xaml.cs
+++ WpfApplication1/ScoreOfStudents.xaml.cs
@@ -3,23 +3,20 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApplication1 {
   /// <summary>
   /// Interaction logic for ScoreOfStudents.xaml
   /// </summary>
   public partial class ScoreOfStudents : UserControl {
+
+    public List<string> InitialMaleStudentsScores { get; set; } = new List<string>();
+
+    public List<string> InitialFemaleStudentsScores { get; set; } = new List<string>();
 
     public ObservableCollection<StudentWithScore> MaleStudentsWithScores { get; set; } = new ObservableCollection<StudentWithScore>();
     public ObservableCollection<StudentWithScore> FemaleStudentsWithScores { get; set; } = new ObservableCollection<StudentWithScore>();
@@ -28,6 +25,22 @@ namespace WpfApplication1 {
     public ICollectionView FemaleStudentsView { get; }
 
     public EventHandler SaveScores;
+
+    private string _type;
+    public string Type 
+    {
+      get { return _type; }
+      set { tbType.Text = _type = value; }
+    }
+
+    private int _highestScore;
+    public int HighestScore 
+    { 
+      get { return _highestScore; }
+      set { tbHighestScore.Text = "Highest Score: " + (_highestScore = value).ToString(); }
+    }
+
+    public uint ColumnIndex { get; set; }
 
     public ScoreOfStudents() {
       InitializeComponent();
@@ -62,8 +75,7 @@ namespace WpfApplication1 {
       int index = (int)tb.Tag; // index in the collection
 
       string newValue = tb.Text;
-      string oldValue = MaleStudentsWithScores[index].Score;
-
+      string oldValue = InitialMaleStudentsScores[index];
       btnSaveScores.IsEnabled = (newValue != oldValue);
     }
     private void FemaleStudentScoreTextChanged(object sender, TextChangedEventArgs e) {
@@ -71,13 +83,27 @@ namespace WpfApplication1 {
       int index = (int)tb.Tag; // index in the collection
 
       string newValue = tb.Text;
-      string oldValue = FemaleStudentsWithScores[index].Score;
-
+      string oldValue = InitialFemaleStudentsScores[index];
       btnSaveScores.IsEnabled = (newValue != oldValue);
     }
 
     private void LettersOnlyTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e) {
       e.Handled = !e.Text.All(c => char.IsLetter(c) || c == ',' || c == ' ' || c == '.');
+    }
+
+    private void NumberOnlyTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e) {
+      var tb = (TextBox)sender;
+
+      // Predict what the text will look like after this input
+      string newText = tb.Text.Insert(tb.SelectionStart, e.Text);
+
+      if (int.TryParse(newText, out int value)) {
+        if (value > HighestScore) {
+          e.Handled = true; // block the input
+        }
+      } else {
+        e.Handled = true; // block non-numeric input
+      }
     }
   }
 
